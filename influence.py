8db3b46996409f8ad1a7f78691b552ce354763be
saman <samanf74@gmail.com>
2016-12-17 17:38:41 +0330

    a lil bug fixed

---
+++ influence.py
@@ -0,0 +1,72 @@
+import copy
+import readData as rd
+
+initialState,connect = rd.getData()
+maxMoves = 20
+states = []
+lastRoundStates = []
+lastRoundStates.append(initialState)
+
+for moves in range(maxMoves):
+	notRepeatedState = []
+	newStates = []
+	
+	for i in range(len(lastRoundStates)):
+		isExist = 0
+		for j in range(len(states)):
+			if lastRoundStates[i][0] == states[j][0]:
+				isExist = 1;
+				break;
+		if isExist == 0:
+			notRepeatedState.append(lastRoundStates[i])
+	
+	for i in range(len(notRepeatedState)):
+		for j in range(len(connect)):
+			tmp1 = copy.deepcopy(notRepeatedState[i])
+			tmp2 = tmp1[0]
+			for k in range(len(tmp2)):
+				if connect[j][k] == 1:
+					tmp2 = tmp2[:k]+ str(((int(tmp2[k])+1)%2)) +tmp2[k+1:]
+			
+			#print(tmp1[0]," > ",tmp2)
+			tmp1[1].append(j)
+			if '0' not in tmp2:
+				print()
+				print("result > ",tmp1[1])
+				exit(1)
+			
+			newStates.append([tmp2,tmp1[1]])
+	
+	tmp=[]
+	for i in range(len(newStates)):
+		isExist = 0
+		for j in range(i):
+			if newStates[i][0] == newStates[j][0]:
+				isExist = 1
+		if isExist == 0:
+			tmp.append(newStates[i])
+	
+	newStates = tmp
+		
+	
+		
+	for i in lastRoundStates:
+		states.append(i)
+		
+	lastRoundStates= []
+	
+	
+	print("number of old states: ",len(states)," number of new states: ",len(newStates))
+	
+	
+	for i in range(len(newStates)):
+		isExist = 0
+		for j in range(len(states)):
+			if newStates[i][0] == states[j][0]:
+				isExist = 1;
+				if len(newStates[i][1]) < len(states[j][1]):
+					states[j][1] = newStates[j][1]
+				break
+				
+		if isExist == 0:
+			lastRoundStates.append(newStates[i])
---
+++ readData.py
@@ -0,0 +1,24 @@
+import json
+def getData():
+	try:
+		path = input("Enter path to data: ")
+		with open(path) as data_file:    
+			data = json.load(data_file)
+			n = int(data["number"])
+			initial = data["initial"]
+			conncet = [[0 for i in range(n)] for j in range(n)]
+			for i in range(n):
+				list = data["connect"][i][str(i)]
+				for j in list:
+					conncet[i][j] = 1
+		
+			print("Data file matrix:")
+			print(conncet)
+			print()
+			
+			initialState = [initial,[]]
+			return initialState,conncet
+			
+	except:
+		print("error in reading data")
+		exit(1)
